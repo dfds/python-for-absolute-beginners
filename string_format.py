68b1506f308ed556ce0916163af2ff2df328169b
Audun Nes <aunes@dfds.com>
2021-05-26 11:01:26 +0200

    Make sure to use python3 when running these files.

--- string_format.py
+++ string_format.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 title: str = 'Lady'
 name: str = 'Gaga'
--- type_hints.py
+++ type_hints.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Without type hints. Previously the IDEs would not be able to see that
 # these were str, int and list.
